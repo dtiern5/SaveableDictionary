d8a4934d2b55caad3d3b1ba585133ad7615038aa
dtiern5 <dtierney@fastmail.com>
2021-04-24 19:02:07 -0500

    SaveableDictionary now contains two hashmaps, added load()

--- src/main/java/dictionary/SaveableDictionary.java
+++ src/main/java/dictionary/SaveableDictionary.java
@@ -1,38 +1,78 @@
 package dictionary;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class SaveableDictionary {
 
-    private HashMap<String, String> dictionary;
+    private HashMap<String, String> englishToFinnish;
+    private HashMap<String, String> finnishToEnglish;
+    private String file;
 
     public SaveableDictionary() {
-        this.dictionary = new HashMap<>();
+        this.englishToFinnish = new HashMap<>();
+        this.finnishToEnglish = new HashMap<>();
+    }
+
+    public SaveableDictionary(String file) {
+        this();
+        this.file = file;
+    }
+
+    public boolean load() {
+
+        try (Scanner fileReader = new Scanner(file)) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");
+                String newWord = parts[0];
+                String newTranslation = parts[1];
+                finnishToEnglish.put(newWord, newTranslation);
+                englishToFinnish.put(newTranslation, newWord);
+            }
+        } catch (Exception e) {
+            System.out.println("Error in load() method: " + e);
+            return false;
+        }
+
+        return true;
     }
 
     public void add(String word, String translation) {
-        if (!dictionary.containsKey(word)) {
-            dictionary.put(word, translation);
+        if (!finnishToEnglish.containsKey(word)) {
+            finnishToEnglish.put(word, translation);
         }
-        if (!dictionary.containsValue(word)) {
-            dictionary.put(translation, word);
+        if (!englishToFinnish.containsKey(translation)) {
+            englishToFinnish.put(translation, word);
         }
+
     }
-    
+
     public void delete(String word) {
-        String wordTranslated = dictionary.get(word);
-        dictionary.remove(word);
-        dictionary.remove(wordTranslated);
+        if (englishToFinnish.containsKey(word)) {
+            String translation = englishToFinnish.get(word);
+            englishToFinnish.remove(word);
+            finnishToEnglish.remove(translation);
+        }
+        if (finnishToEnglish.containsKey(word)) {
+            String translation = finnishToEnglish.get(word);
+            finnishToEnglish.remove(word);
+            englishToFinnish.remove(translation);
+        }
+
     }
 
     public String translate(String word) {
-            return dictionary.get(word);
-    }
-    
-    public void printDict() {
-        for (String word : dictionary.keySet()) {
-            System.out.println(word);
+        String translation = null;
+        if (englishToFinnish.containsKey(word)) {
+            translation = englishToFinnish.get(word);
         }
+        if (finnishToEnglish.containsKey(word)) {
+            translation = finnishToEnglish.get(word);
+        }
+        return translation;
     }
 
 }
